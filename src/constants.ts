7c838fc87439d5cb6c41c19ada2fb6e260b88b22
Anandhu Vijayan <anandhuvijayank@gmail.com>
2022-02-27 20:30:53 +0530

    constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,7 @@
 export default class AppConstant {
-    public static MONGO_URL = "mongodb://localhost:27017"
-    public static DB_NAME = "lodestareduinternational"
-
-  
-
-
+    public static MONGO_URL = "mongodb://localhost:27017";
+    public static DB_NAME = "lodestareduinternational";
+    public static ALL_CONST = '000000000000000000000000';
 }
 
 export module AppConstant { }
