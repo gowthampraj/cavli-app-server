76d809cdb1f2d78fba48ea5e0eaab84158770ac3
Anandhu Vijayan <anandhuvijayank@gmail.com>
2022-04-14 13:28:16 +0530

    model

--- src/models/course.model.ts
+++ src/models/course.model.ts
@@ -4,11 +4,13 @@ export class CourseModel {
     country?: string[];
     uni?: string[]; // universities
     intakes?: string[];
+    comment?: string;
     constructor(i: CourseModel) {
         if (i._id) this._id = i._id;
         this.name = i.name;
         this.country = i?.country ?? [];
         this.uni = i.uni ?? [];
         this.intakes = i?.intakes ?? [];
+        this.comment = i.comment;
     }
 }
